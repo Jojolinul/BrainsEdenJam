e88aff9ca5028ce5e10429b1040c6cc073d3583f
MrMobble <mobbs.oliver@googlemail.com>
2018-07-14 11:30:18 +0100

    V1.0

---
+++ Source/MyProject/W_Pickup.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "W_Pickup.generated.h"
+
+UCLASS()
+class MYPROJECT_API UW_Pickup : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void IniButton(FVector Location);
+	
+	
+};
