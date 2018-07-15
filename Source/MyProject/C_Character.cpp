e58b0600ade1ead9745c443c0e6c43c870124f1c
MrMobble <mobbs.oliver@googlemail.com>
2018-07-15 15:39:08 +0100

    Commit

--- Source/MyProject/C_Character.cpp
+++ Source/MyProject/C_Character.cpp
@@ -225,8 +225,6 @@ void AC_Character::PickUp()
 		const AActor* _OtherActor = Cast<AA_Pickup>(TraceResult.GetActor());
 		if (_OtherActor)
 		{
-			if (!CurrentItem) Durability -= 10.0f;
-
 			CurrentItem = TraceResult.GetActor();
 			CurrentItem->SetActorLocationAndRotation(GetMesh()->GetSocketTransform("Arms").GetLocation(), GetMesh()->GetSocketTransform("Arms").GetRotation());
 			Cast<UPrimitiveComponent>(CurrentItem->GetRootComponent())->SetSimulatePhysics(false);
